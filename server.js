4a9ff29c354572a53d12da15e60915d5b44c346e
TristanGavin <gavin.tristan@gmail.com>
2019-12-11 12:40:11 -0800

    Adjusted the handlebars to serve new json file.

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('/index.html', function(req, res, next){
 app.get('/recipes.html', function(req, res, next){
     console.log(req.url, 'success')
     res.render('recipe', {
-        recipeData: recipeData
+        recipes: recipeData.recipes
     })
 });
 
